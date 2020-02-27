68af9a6b3fb7938f8a29530460c2aad7382f45db
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-02-27 16:20:38 +0300

    Add animated bars (+ remove them dynamicly)

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const margin = {
 }
 
 // Define width and height of the working place with margin (bar)
-const width = 900 - margin.left - margin.right;
-const height = 900 - margin.top - margin.bottom;
+const width = 400 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
 
 // Create an svg element
 const svg = d3.select("body").append("svg")
@@ -23,39 +23,70 @@ const data = [
     { id: 2, title: 'hello1', value: 2 },
     { id: 3, title: 'hello2', value: 102 },
     { id: 4, title: 'hello3', value: 312 },
+    { id: 5, title: 'hello4', value: 123 },
 ];
 
 
 const draw = () => {
 
+    const barHeight = 50;
+    const barOffset = 3;
+
     const bars = svg.selectAll(".bar").data(data);
 
+    bars.exit()
+        .transition()
+        .duration(1000)
+            .style("fill", "rgb(243, 47, 47)")
+            .style("opacity", "0")
+            .remove();
+
     // Add new items initially
-    bars
+    const addBars = bars
         .enter()
-            .append("rect")
-                .attr("class", "bar new")
-                .attr("height", 100)
-                .attr("width", (d) => d.value)
-                .attr("y", (d, n) => n * 100);
+        .append("rect")
+        .attr("class", "bar")
+        .attr("height", barHeight);
 
+    //Update items every time draw is colling, comparing with existed values
+    addBars.merge(bars)
+        .transition()
+        .duration(1000)
+        .style("fill", "rgb(96, 159, 211)")
+        .attr("width", (d) => d.value)
+        .attr("y", (d, n) => n * barHeight + n * barOffset);
 
-    // Update items every time draw is colling
-    bars
-        .attr("class", "bar updated")
-        .attr("width", (d) => d.value )
-        .attr("y", (d,n) => n * 100 );
+    // // Update items every time draw is colling
+    // bars
+    //     .attr("class", "bar updated")
+    //     .attr("width", (d) => d.value)
+    //     .attr("y", (d, n) => n * 100);
 };
-
 
 draw();
 
-// Imitate dynamicly update
-setTimeout(() => {
-        data[1].value = 256;
-        data[2].value = 350;
-        draw();
-}, 2000);
+
+// Imitate dynamicly update with timer
+// setTimeout(() => {
+
+//     console.log("-----");
+
+//     data[1].value = 256;
+//     data[0].value = 154;
+//     data.splice(4, 1);
+//     console.log(data);
+    
+//     draw();
+
+// }, 2000);
+
+
+onChangeBar = () => {
+    data[1].value = 256;
+    data[0].value = 154;
+    data.splice(4, 1);
+    draw();
+}
 
 
 
