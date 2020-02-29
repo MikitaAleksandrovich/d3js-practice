b5b6997abd7b831d601cb1ee961a01ec599fbaf0
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-02-29 11:38:22 +0300

    Implement crypto currency values barchart
    + add server  to avoid problems with requests

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
 // Margin conversation 
 const margin = {
     top: 50,
-    bottom: 50,
-    left: 50,
     right: 50,
-}
+    bottom: 50,
+    left: 50
+};
 
 // Define width and height of the working place with margin (bar)
 const width = 900 - margin.left - margin.right;
 const height = 900 - margin.top - margin.bottom;
 
 // Create an svg element
-const svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("tranform", `translate(${margin.left}, ${margin.top})`);
-
-
-const data = [
-    { id: 1, title: 'hello', value: 22 },
-    { id: 2, title: 'hello1', value: 2 },
-    { id: 3, title: 'hello2', value: 102 },
-    { id: 4, title: 'hello3', value: 1000 },
-    { id: 5, title: 'hello4', value: 123 },
-];
-
-
+const svg = d3.select('body').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 // Set x scaler and axis
 const x = d3.scaleLinear()
     .range([0, width]);
 
-const xAxis = d3.axisBottom()
+const xAxis = d3.axisTop()
     .scale(x);
 
 svg
-    .append("g")
-    .attr("class", "axis-x")
+    .append('g')
+    .attr('class', 'axis-x')
     .call(xAxis);
 
 
@@ -45,92 +34,88 @@ svg
 const y = d3.scaleBand()
     .range([0, height]);
 
-const yAxis = d3.axisRight()
-    .tickPadding([10])
+const yAxis = d3.axisLeft()
     .scale(y);
 
-svg
-    .append("g")
-    .attr("class", "axis-y")
+svg.append('g')
+    .attr('class', 'axis-y')
     .call(yAxis);
 
 
+const draw = (data) => {
 
-const draw = () => {
-
-    const barHeight = 50;
+    const barHeight = 70;
     const barOffset = 3;
-
-    const bars = svg.selectAll(".bar").data(data);
 
     // Fix maximum width value, based on value of the most wide item
     const valueRange = [
         0,
-        d3.max(data, (d) => d.value)
+        d3.max(data, d => d.value + 300)
     ];
 
     x.domain(valueRange);
-
-    y.domain(data.map(item => item.title))
+    y.domain(data.map(d => d.title))
         .range([0, data.length * barHeight + data.length * barOffset - barOffset]);
 
-    // Add new items initially
-    const addBars = bars
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("height", barHeight);
+    const bars = svg.selectAll('.bar').data(data);
 
-    // Remove bars 
-    bars.exit()
+    bars
+        .exit()
         .transition()
         .duration(1000)
-        .style("fill", "rgb(243, 47, 47)")
-        .style("opacity", "0")
+        .attr('width', 0)
+        .style('fill', 'red')
         .remove();
 
-    //Update items every time draw is colling, comparing with existed values
+    const addBars = bars
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('height', barHeight);
+
+
     addBars.merge(bars)
         .transition()
         .duration(1000)
-        .style("fill", "rgb(96, 159, 211)")
-        .attr("width", (d) => x(d.value))
-        .attr("y", (d, n) => n * barHeight + n * barOffset);
+        .attr('width', d => x(d.value))
+        .attr('y', (d, n) => n * barHeight + n * barOffset);
 
-    // // Update items every time draw is colling
-    // bars
-    //     .attr("class", "bar updated")
-    //     .attr("width", (d) => d.value)
-    //     .attr("y", (d, n) => n * 100);
-
-    svg.select(".axis-x")
+    svg
+        .select('.axis-x')
         .transition()
         .call(xAxis);
 
-    svg.select(".axis-y")
+    svg
+        .select('.axis-y')
         .transition()
         .call(yAxis);
-};
-
-draw();
+}
 
 
-// Imitate dynamicly update with timer
+
+
+const updateCrypto = () => Promise.all([
+    d3.json('http://localhost:8005/api/2/btc_usd/depth')
+        .then(d => ({ name: 'btc', ...d })),
+    d3.json('http://localhost:8005/api/2/ltc_usd/depth')
+        .then(d => ({ name: 'ltc', ...d })),
+    d3.json('http://localhost:8005/api/2/eth_usd/depth')
+        .then(d => ({ name: 'eth', ...d })),
+]).then(result => {
+    data = result.map((depth, n) => ({ id: n, title: depth.name, value: depth.bids[0][0] }));
+    draw(data);
+    console.log("Updated!");
+});
+
 setInterval(() => {
-
-    const elementNum = Math.round(Math.random() * 4);
-    data[elementNum].value = Math.round(Math.random() * 800);
-    // const elementNumToDelete = Math.round(Math.random() * 4);
-
-    draw();
-
+    updateCrypto();
 }, 1000);
 
 
 // onChangeBar = () => {
 //     data[1].value = 256;
 //     data[0].value = 154;
-//     data.splice(4, 1);
+//     data.splice(2, 1);
 //     draw();
 // }
 
